2d671b3c8238ccaee7f24c3c38b36fba7ba7598e
SamixDev <samer.hd.ah@gmail.com>
2023-11-21 11:45:57 +0100

    change testnet addr in deploy

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -22,22 +22,22 @@ async function main() {
   await optionsInstance.setUpdater(updaters);
   await new Promise(r => setTimeout(r, 5000));
   await optionsInstance.addToken({
-    name: "chainlink", tokenAddress: "0x326C977E6efc84E512bB9C30f76E30c160eD06FB", isAllowed: true, symbol: "LINK", decimals: 18,
+    name: "chainlink", tokenAddress: "0x9DFf9E93B1e513379cf820504D642c6891d8F7CC", isAllowed: true, symbol: "LINK", decimals: 18,
     priceFeedAddress: "0x1C2252aeeD50e0c9B64bDfF2735Ee3C932F5C408", isStable: false
   });
   await new Promise(r => setTimeout(r, 5000));
   await optionsInstance.addToken({
-    name: "Usdc", tokenAddress: "0xe6b8a5CF854791412c1f6EFC7CAf629f5Df1c747", isAllowed: true, symbol: "USDC", decimals: 18,
+    name: "usdc", tokenAddress: "0x52D800ca262522580CeBAD275395ca6e7598C014", isAllowed: true, symbol: "USDC", decimals: 18,
     priceFeedAddress: "0x572dDec9087154dC5dfBB1546Bb62713147e0Ab0", isStable: true
   });
   await new Promise(r => setTimeout(r, 5000));
   await optionsInstance.addToken({
-    name: "bitcoin", tokenAddress: "0x0d787a4a1548f673ed375445535a6c7A1EE56180", isAllowed: true, symbol: "WBTC", decimals: 18,
+    name: "bitcoin", tokenAddress: "0x2Fa2e7a6dEB7bb51B625336DBe1dA23511914a8A", isAllowed: true, symbol: "WBTC", decimals: 18,
     priceFeedAddress: "0x007A22900a3B98143368Bd5906f8E17e9867581b", isStable: false
   });
   await new Promise(r => setTimeout(r, 5000));
   await optionsInstance.addToken({
-    name: "ethereum", tokenAddress: "0xA6FA4fB5f76172d178d61B04b0ecd319C5d1C0aa", isAllowed: true, symbol: "WETH", decimals: 18,
+    name: "ethereum", tokenAddress: "0xc199807AF4fEDB02EE567Ed0FeB814A077de4802", isAllowed: true, symbol: "WETH", decimals: 18,
     priceFeedAddress: "0x0715A7794a1dc8e42615F059dD6e406A6594651A", isStable: false
   });
 
